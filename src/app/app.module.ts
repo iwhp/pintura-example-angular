3f88844b7d4fb79dab88e5c18164b639dc38ffb0
Rik <rik@pqina.nl>
2023-05-10 11:58:59 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AngularPinturaModule } from '@pqina/angular-pintura';
+
+import { FilePondModule, registerPlugin } from 'ngx-filepond';
+import * as FilePondPluginFilePoster from 'filepond-plugin-file-poster';
+import * as FilePondPluginImageEditor from '@pqina/filepond-plugin-image-editor';
+
+// @ts-ignore
+registerPlugin(FilePondPluginFilePoster, FilePondPluginImageEditor.default);
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularPinturaModule, FilePondModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
